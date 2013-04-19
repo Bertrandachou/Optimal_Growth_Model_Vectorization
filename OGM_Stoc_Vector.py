44ff533decf31bf0315432115defca76f929a423
Bertrand Achou <bertrandachou@orange.fr>
2013-04-19 16:21:49 +0200

    V3
    Still not working but clearer

--- OGM_Stoc_Vector.py
+++ OGM_Stoc_Vector.py
@@ -81,71 +81,37 @@ def new_value(k,kp,V0,p,pgrid,A,shock):
     
     new_V0 = np.array([ [ np.zeros(pgrid['n']) for k in xrange(pgrid['n']) ] for j in xrange(len(shock['A']))] )   
 
-    #new_V00            = np.zeros(pgrid['n'])    
-    #new_V01            = np.zeros(pgrid['n']) 
-    
-    
-    
-    #DV00               = np.array([[V00[i] for j in range(pgrid['n'])] for i in range(pgrid['n'])])
-    #DV01               = np.array([[V01[i] for j in range(pgrid['n'])] for i in range(pgrid['n'])])                
-    
     budget_not           = ((production(k,p,A) + (1 - p['delta']) * k - kp) < 0)
-    
-    #budget_not0         = ((production(k,p,shock['A'][0]) + (1 - p['delta']) * k - kp) < 0)  # looks if the constraint is NOT satisfied
-    #budget_not1         = ((production(k,p,shock['A'][1]) + (1 - p['delta']) * k - kp) < 0)
-    
-
-    
-    #DV00[budget_not0]   = -999999999999  #gives a large penalty no to satisfy the constraint
-    #DV01[budget_not1]   = -999999999999
-    
     
     ctemp               = production(k,p,A) + (1 - p['delta']) * k - kp
     
-    #ctemp0             = production(k,p,shock['A'][0]) + (1 - p['delta']) * k - kp
-    #ctemp1             = production(k,p,shock['A'][1]) + (1 - p['delta']) * k - kp
-    
-    
-    ctemp [budget_not]  = 0.01
-    
-    
-    #ctemp0[budget_not0] = 0.001          #gives a large penalty no to satisfy the constraint
-    #ctemp1[budget_not1] = 0.001
-    
-    
+    ctemp[budget_not]  = 0.001  
+   
     utemp               = utility(ctemp,p)
+    
+    TV0                 = np.array([ [ np.linspace(V0[i,0,k],V0[i,0,k],pgrid['n']) for k in xrange(pgrid['n']) ] for i in xrange(len(shock['A'])) ] )
     
     for i in xrange(len(shock['A'])):
         
-        utemp[i] += sum( [shock['transit'][i,j]*V0[j] for j in range(len(shock['A'])) ] )
-    
-    
-    #Vtemp0             = utility(ctemp0,p) + p['beta'] * ( shock['transit'][0,0]*DV00 + shock['transit'][0,1]*DV01 )
-    #Vtemp1             = utility(ctemp1,p) + p['beta'] * ( shock['transit'][1,0]*DV00 + shock['transit'][1,1]*DV01 )
+        utemp[i] = utemp[i] + p['beta'] * (sum( [shock['transit'][i,j]*TV0[j] for j in range(len(shock['A'])) ] ))
     
     
     utemp[budget_not] = -99999999
     
     Vtemp        = utemp.reshape(len(shock['A']),pgrid['n']*pgrid['n'])
     
-    # Vtemp02            = Vtemp0.reshape(1,pgrid['n']*pgrid['n'])
-    #Vtemp12            = Vtemp1.reshape(1,pgrid['n']*pgrid['n'])
+
 
     for i in xrange(len(shock['A'])):
         
         for j in xrange(pgrid['n']):
             
-            new_V0[i,j,:] = max(Vtemp[i][j::pgrid['n']])
-        
-        #new_V00[i] = max(Vtemp02[0][i::pgrid['n']])
-        #new_V01[i] = max(Vtemp12[0][i::pgrid['n']])
-        
+            new_V0[i,:,j] = max(Vtemp[i][j::pgrid['n']])
+            
         
         
     return new_V0
     
-
-
 
 
 """ Now we run the algorithm """
@@ -163,24 +129,15 @@ while crit > epsilon:
     
     V0        = grid_new
     
-    
-    #grid_new = new_value(kgrid,kpgrid,V00,V01,p,pgrid,shock)
-    #V0       = grid_new[0]
-    #V1       = grid_new[1]
-    #crit1    = max(abs(V0-V00))
-    #crit2    = max(abs(V1-V01))
-    #crit     = max([crit1,crit2])
-    #V00 = V0
-    #V01 = V1
 
 
 
 
     
-import pylab
+#import pylab
 
-kgrid2 = np.linspace(pgrid['kmin'],pgrid['kmax'],pgrid['n'])
+#kgrid2 = np.linspace(pgrid['kmin'],pgrid['kmax'],pgrid['n'])
    
-pylab.plot(kgrid2,V0[0,:,0])
+#pylab.plot(kgrid2,V0[0,:,0])
 #pylab.plot(kgrid2,V01)
 
